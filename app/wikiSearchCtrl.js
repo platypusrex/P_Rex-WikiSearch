bf96736d4f7f020af1b3b414a05f77b700ce6a38
platypusrex <frankcooke79@gmail.com>
2015-11-17 15:02:40 -0500

    added the controller for the app's main module

---
+++ app/wikiSearchCtrl.js
@@ -0,0 +1,26 @@
+(function(wikiSearch){
+    wikiSearch.controller('mainController', ['$scope', 'searchService', function($scope, searchService){
+
+        var onSuccess = function(data){
+            $scope.wikiPages = data.query.search.map(function(val){
+                return {
+                    snippet: val.snippet + '<em>....click anywhere to read more</em>',
+                    timeStamp: val.timestamp,
+                    title: val.title,
+                    link: 'https://en.wikipedia.org/wiki/' + val.title
+                }
+            });
+            console.log($scope.wikiPages);
+        };
+
+        var onError = function(err){
+            $scope.err = 'Sorry, dude. Looks like there was a problem with the request. Try again in a minute.'
+        };
+
+        $scope.getWikiResults = function(inputValue){
+            searchService.searchWikipedia(inputValue).then(onSuccess, onError);
+        };
+
+        console.log($scope.wikiPages);
+    }]);
+}(angular.module('WikiSearchApp')));
